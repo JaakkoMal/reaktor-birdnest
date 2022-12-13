89736193a6b807967f51d5d55ab46161e2ebb070
JaakkoMal <j.j.malmi@gmail.com>
2022-12-13 20:59:25 +0200

    Just added a bit of CSS for own amusement

--- dronebuster/src/components/DroneInfo.js
+++ dronebuster/src/components/DroneInfo.js
@@ -4,21 +4,26 @@ export default function DroneInfo({drones, badGuys}) {
 
   
   return (
-
-      <div>
-          <h1>Reaktor Birdnest assignment</h1>
+    <div className='container'>
+      <nav>
+        <h1>Reaktor Birdnest Pre-assignment</h1>
+      </nav>
+     <div className='droneInfoContainer'>
           {
             drones.sort((drone1, drone2) => new Date(drone2.timestamp) - new Date(drone1.timestamp)).map(drone => (
-              <ul key={drone.serialNumber}>
-                      <p>{drone.model}</p>
-                      <li>{drone.serialNumber}</li>
-                      <li>Last seen: {drone.timestamp}</li>
-                      <li>Last measured distance: {drone.distanceFromTheNest.toFixed(2)}m</li>
-                      <li>Closest distance: {drone.closestDistance.toFixed(2)}m</li>
-                  </ul>
+              <div className='infoBox'> 
+                <ul key={drone.serialNumber}>
+                        <h3>{drone.model}</h3>
+                        <li>Serial number: <b>{drone.serialNumber}</b></li>
+                        <li>Last spotted: <b>{drone.timestamp}</b></li>
+                        <li>Last distance: <b>{drone.distanceFromTheNest.toFixed(2)}m</b></li>
+                        <li>Closest to the nest: <b>{drone.closestDistance.toFixed(2)}m</b></li>
+                    </ul>
+              </div>      
             ))
           }       
       </div>
+     </div> 
 
   )
 }
