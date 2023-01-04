47ea159bd9a782e4977c0eb7571575e9cd3ed64b
JaakkoMal <j.j.malmi@gmail.com>
2023-01-04 17:07:52 +0200

    Added visualization
    User can now choose to see the real time situation on a radar screen.

---
+++ dronebuster/src/components/Visualization.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Chart as ChartJS, LinearScale, PointElement, LineElement, Tooltip, Legend,} from 'chart.js';
+import { Scatter } from 'react-chartjs-2';
+
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip);
+
+export default function Visualization({drones}) {
+
+    const options = {
+        animation: false,
+        aspectRatio: 1,
+        scales: {
+          y: {
+            min: 0,
+            max: 500000,
+            grid: {
+                color: 'green'
+            },
+            ticks: {
+                display: false
+            }
+          },
+          x: {
+            min: 0,
+            max: 500000,
+            grid: {
+                color: 'green'
+            },
+            ticks: {
+                display: false
+            }
+          },
+        },
+        elements: {
+            point: {
+                pointStyle: 'rectRot',
+                radius: 5
+            }
+        }
+      };
+
+    const dronePositions = [
+        {
+            label: 'THE NEST',
+            data: [{
+                x: 250000,
+                y: 250000
+            }],
+            backgroundColor: 'green'
+        }
+    ]
+    drones.forEach(drone => {
+        dronePositions.push({
+            label: drone.model,
+            data: [{
+                x: Number(drone.positionX),
+                y: Number(drone.positionY)
+            }],
+            backgroundColor: 'rgba(255, 99, 132, 1)',
+        })
+    })
+
+    const data = {
+        datasets: dronePositions,
+      };
+    
+    return (
+        <Scatter options={options} data={data} />
+    )
+  }
+
+
